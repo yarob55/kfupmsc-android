1b15a30aec1fbfb822e1ef91259fc0237c457173
abdulrhmanagha <abdulrhmanagha96@gmail.com>
2017-12-10 22:01:14 +0300

    change 1

---
+++ app/src/main/java/agha/kfupmtest/MainActivity.java
@@ -0,0 +1,15 @@
+package agha.kfupmtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // change 1
+}
